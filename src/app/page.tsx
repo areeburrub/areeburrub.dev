8ebafee334044b000934825befb125d16c95d1cb
areeburrub <areeburrub@gmail.com>
2023-05-24 22:53:27 +0530

    init: v1 portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,245 @@
 import Image from 'next/image'
+import { Roboto_Mono } from 'next/font/google'
+import {
+  RiArrowRightSLine, RiFile2Fill,
+  RiFolderFill,
+  RiGithubFill,
+  RiLinkedinFill,
+  RiTwitterFill
+} from "react-icons/ri";
+import Link from "next/link";
+import Marquee from "react-fast-marquee";
+
+const roboto = Roboto_Mono({
+  weight: ['400'],
+  subsets: ['greek'],
+})
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import {cn} from "@/lib/utils";
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex text-xl min-h-screen flex-col items-center gap-10 pb-10">
+
+      <div className={"container grid grid-cols-12 w-full h-[60vh] mt-10"}>
+        <div className={"col-span-12 md:col-span-6 w-full flex flex-col items-start justify-center gap-2"}>
+          <h3 className={roboto.className + " text-purple text-2xl"}>Hi I am Areeb ur Rub</h3>
+          <h1 className={'text-5xl font-semibold leading-tight'}>
+            Full Stack Developer and <br/>
+            Graphic Designer
+          </h1>
+          <p className={"text-md font-normal"}>
+            Passionate web developer and graphic designer learning web3 with Solidity. Passionate about creating intuitive and user-friendly experiences and bringing visual designs to life.
+          </p>
+          <div className={"flex-row gap-2 text-3xl mt-6 flex"}>
+            <Link href={"#"}>
+              <RiGithubFill/>
+            </Link>
+            <Link href={"#"}>
+              <RiLinkedinFill/>
+            </Link>
+            <Link href={"#"}>
+              <RiTwitterFill/>
+            </Link>
+          </div>
+        </div>
+        <div className={"col-span-12 md:col-span-6 w-full flex flex-col items-center justify-center"}>
+          <div className={"bg-gray-800 w-full h-full bg-opacity-50 "}>
+            <img alt={"Areeb ur Rub"} src={'/hero.JPEG'} className={' w-full max-h-[60vh] object-cover'}/>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={'w-full h-[14rem] mt-16 overflow-hidden'}>
+      <div className={'w-full flex flex-col justify-center bg-violet mt-16'}>
+        <div className={'w-full bg-violet-dark py-5 border-b-2 border-t-2 border-sky2 -rotate-3'}>
+          <Marquee>
+            <div className={"w-full flex-row flex justify-evenly items-center gap-5 px-2.5"}>
+              <span className={roboto.className + " font-normal text-md"}>CSS</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>JavaScript</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>TypeScript</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>Python</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>Node.js</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>React.js</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>Tailwind CSS</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>Next.js</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>Flask</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>C</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>C++</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>Figma</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>Illustrator</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>HTML</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>C</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>C++</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>Figma</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>Illustrator</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+              <span className={roboto.className + " font-normal text-md"}>HTML</span>
+              <div className={'w-2 h-2 rounded-full bg-sky'}></div>
+            </div>
+          </Marquee>
+        </div>
+      </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <div className={"container grid grid-cols-12 w-full h-[60vh] mt-10"}>
+        <div className={"col-span-12 md:col-span-6 w-full flex flex-col items-start justify-center gap-2"}>
+          <h3 className={roboto.className + " text-purple text-2xl"}>About Me</h3>
+          <h1 className={'text-5xl font-semibold leading-tight'}>
+            I am a Computer <br/>Science Undergrad
+          </h1>
+          <p className={"text-md font-normal mr-auto md:mr-10"}>
+            I am passionate about learning and leveraging technology. I have honed my skills in creating websites and applications using React, Next JS, Flask, and Tailwind CSS. Currently, I&apos;m delving into the exciting realms of web3 and Solidity for decentralized app development, while also nurturing my interests in machine learning and Android.
           </p>
-        </a>
+        </div>
+        <div className={"col-span-12 md:col-span-6 w-full flex flex-col items-center justify-center"}>
+          <Accordion type="single" collapsible className={'w-full'}>
+            <AccordionItem value="item-1">
+              <AccordionTrigger className={roboto.className + " my-2 bg-violet-dark border-2 rounded-xl flex flex-row items-center justify-start p-2 gap-2 border-purple w-full"}>
+                  <RiArrowRightSLine className={'text-white'}/>
+                  <div className={'flex flex-row items-center justify-center gap-5'}>
+                    <RiFolderFill className={'text-purple text-2xl'}/>
+                    <span className={'text-white text-md'}>Programming Languages</span>
+                  </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className={'w-full grid grid-cols-2 gap-x-10 gap-y-4 text-xl mx-20 my-2'}>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> Python
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> JavaScript
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> TypeScript
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> C
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> C++
+                  </span>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger className={roboto.className + " my-2 bg-violet-dark border-2 rounded-xl flex flex-row items-center justify-start p-2 gap-2 border-purple w-full"}>
+                  <RiArrowRightSLine className={'text-white'}/>
+                  <div className={'flex flex-row items-center justify-center gap-5'}>
+                    <RiFolderFill className={'text-purple text-2xl'}/>
+                    <span className={'text-white text-md'}>Frameworks</span>
+                  </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className={'w-full grid grid-cols-2 gap-x-10 gap-y-4 text-xl mx-20 my-2'}>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> NextJs
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> ReactJs
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> Express
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> Tailwind CSS
+                  </span>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger className={roboto.className + " my-2 bg-violet-dark border-2 rounded-xl flex flex-row items-center justify-start p-2 gap-2 border-purple w-full"}>
+                  <RiArrowRightSLine className={'text-white'}/>
+                  <div className={'flex flex-row items-center justify-center gap-5'}>
+                    <RiFolderFill className={'text-purple text-2xl'}/>
+                    <span className={'text-white text-md'}>Database</span>
+                  </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className={'w-full grid grid-cols-2 gap-x-10 gap-y-4 text-xl mx-20 my-2'}>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> MongoDB
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> SQLite
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> PostgreSQL
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> FireStore
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> MySQL
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> Realtime Database
+                  </span>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-4">
+              <AccordionTrigger className={roboto.className + " my-2 bg-violet-dark border-2 rounded-xl flex flex-row items-center justify-start p-2 gap-2 border-purple w-full"}>
+                  <RiArrowRightSLine className={'text-white'}/>
+                  <div className={'flex flex-row items-center justify-center gap-5'}>
+                    <RiFolderFill className={'text-purple text-2xl'}/>
+                    <span className={'text-white text-md'}>Designing</span>
+                  </div>
+
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className={'w-full grid grid-cols-2 gap-x-10 gap-y-4 text-xl mx-20 my-2'}>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> Figma
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> Photoshop
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> Illustrator
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> InDesign
+                  </span>
+                  <span className={'col-span-1 flex flex-row items-center gap-2'}>
+                    <RiFile2Fill className={'text-purple text-2xl'}/> MsPaint
+                  </span>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+
+        </div>
       </div>
+
     </main>
   )
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,22 @@
+// @flow
+import * as React from 'react';
+
+export function Navbar() {
+  return (
+    <div className={"container flex flex-row justify-between py-8"}>
+      <div className={"flex flex-row items-end"} aria-details={"Logo"}>
+        <span className={"font-extrabold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-purple to-sky"}>Areeb</span>
+        <span className={"font-regular text-xl"}>.dev</span>
+      </div>
+
+      <div className={"hidden md:flex flex-row items-center gap-10"}>
+        <a href={"#"}>Home</a>
+        <a href={"#"}>Work</a>
+        <a href={"#"}>Project</a>
+        <a href={"#"} className={'px-5 py-2 bg-violet-dark rounded-full border-2 border-sky text-sky hover:border-transparent hover:bg-sky hover:text-violet'}>
+          Contact
+        </a>
+      </div>
+    </div>
+  );
+};
